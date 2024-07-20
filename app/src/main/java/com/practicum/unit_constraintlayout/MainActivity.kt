fd7f18e277b3db945041cd60e76e59f742ba60e1
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2024-07-21 02:01:00 +0300

    init

---
+++ app/src/main/java/com/practicum/unit_constraintlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.practicum.unit_constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_1)
+    }
+}
